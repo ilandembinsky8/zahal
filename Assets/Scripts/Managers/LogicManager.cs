9ce8d1c5b753351e085e1d5989083711e57a39e5
Amit Mor Yosef <amitmoryosef@hotmail.com>
2024-09-24 15:26:53 +0300

    Full Name Search

--- Assets/Scripts/Managers/LogicManager.cs
+++ Assets/Scripts/Managers/LogicManager.cs
@@ -9,13 +9,14 @@ public class LogicManager : MonoBehaviour
     [SerializeField] private List<Casualty> LastKinCasulaties;
     
     private CSVReader csvReader;
-
     private static string _csvLocation => Application.streamingAssetsPath + "\\LastKinList.csv";
     
     private void Start()
     {
-        csvReader = new CSVReader();
-
+        csvReader = new ();
+        
         LastKinCasulaties = csvReader.ReadCSVFile(_csvLocation);
     }
+    
+    
 }
---
+++ Assets/Scripts/Managers/SearchManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+public class SearchManager : MonoBehaviour
+{
+    const string IZKOR_HOMEPAGE = "https://www.izkor.gov.il\\";
+    const string SEARCH_PREFIX = "searchforMemoryCandle\\";
+    
+    private static string FullNameSearchPrefix => IZKOR_HOMEPAGE + SEARCH_PREFIX;
+
+    private void Start()
+    {
+        FullNameSearch("שלו חזן");
+    }
+
+    public void FullNameSearch(string fullNameString)
+    {
+        Application.OpenURL(FullNameStringBuilderUtility(WWW.EscapeURL(fullNameString)));
+    }
+    
+    private string FullNameStringBuilderUtility(string processedString)
+    {
+        Debug.Log($"{FullNameSearchPrefix}{processedString}?full_name={processedString}");
+        return $"{FullNameSearchPrefix}{processedString}?full_name={processedString}";
+    }
+}
