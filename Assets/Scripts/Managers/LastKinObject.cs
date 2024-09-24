e1e9292f29472a8a6e6d2329c3100a1b276f36f6
Amit Mor Yosef <amitmoryosef@hotmail.com>
2024-09-24 19:01:55 +0300

    Last Kin List
    List creation is happening, but not good on visuals

---
+++ Assets/Scripts/Managers/LastKinObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using RTLTMPro;
+using UnityEngine;
+
+public class LastKinObject : MonoBehaviour
+{
+    [SerializeField] private RTLTextMeshPro fullNameText;
+    [SerializeField] private RTLTextMeshPro rankNameText;
+    
+    private Casualty lastKinCasualtyData;
+
+    public void Init(Casualty lastKinCasualtyData)
+    {
+        this.lastKinCasualtyData = lastKinCasualtyData;
+
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        StringBuilder strBuilder = new StringBuilder();
+
+        if (!string.IsNullOrEmpty(lastKinCasualtyData.FirstName))
+        {
+            strBuilder.Append(lastKinCasualtyData.FirstName + " ");
+        }
+        
+        if (!string.IsNullOrEmpty(lastKinCasualtyData.FirstName2))
+        {
+            strBuilder.Append(lastKinCasualtyData.FirstName2 + " ");
+        }
+        
+        if (!string.IsNullOrEmpty(lastKinCasualtyData.NickName))
+        {
+            strBuilder.Append($"({lastKinCasualtyData.NickName}) ");
+        }
+        
+        if (!string.IsNullOrEmpty(lastKinCasualtyData.LastName))
+        {
+            strBuilder.Append(lastKinCasualtyData.LastName + " ");
+        }
+        
+        if (!string.IsNullOrEmpty(lastKinCasualtyData.LastName2))
+        {
+            strBuilder.Append(lastKinCasualtyData.LastName2 + " ");
+        }
+        
+        if (!string.IsNullOrEmpty(lastKinCasualtyData.LastName2))
+        {
+            rankNameText.text = lastKinCasualtyData.LastName2;
+        }
+        else
+        {
+            rankNameText.text = "אין דרגה במסד נתונים";
+        }
+
+        fullNameText.text = strBuilder.ToString();
+
+    }
+    
+}
--- Assets/Scripts/Managers/LogicManager.cs
+++ Assets/Scripts/Managers/LogicManager.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class LogicManager : MonoBehaviour
 {
     [SerializeField] private List<Casualty> LastKinCasulaties;
+    [SerializeField] private GameObject LastKinBoxPrefab;
     
     private CSVReader csvReader;
     private static string _csvLocation => Application.streamingAssetsPath + "\\LastKinList.csv";
@@ -16,7 +17,11 @@ public class LogicManager : MonoBehaviour
         csvReader = new ();
         
         LastKinCasulaties = csvReader.ReadCSVFile(_csvLocation);
+        
+        foreach (var lastKin in LastKinCasulaties)
+        {
+            GameObject go = Instantiate(LastKinBoxPrefab,transform);
+            go.GetComponent<LastKinObject>().Init(lastKin);
+        }
     }
-    
-    
 }
